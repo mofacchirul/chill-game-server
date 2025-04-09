b9f96dec27f85f6d0a28e10c5ea787c73d9ceb0b
mofacchirul <mofaccharulhoque@gmail.com>
2025-04-10 02:39:55 +0600

    midleware

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+ 
 
 
 const upcomming_Collection= client.db("game").collection("upcomming")
@@ -68,9 +68,6 @@ app.get('/player/:id',async(req,res)=>{
 })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
